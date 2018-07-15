17e7c8e9b33f0cba5af325613e5d255f030a2fc3
okm02 <omarkmehio@gmail>
2018-07-15 11:48:33 +0200

    fixed implementation of coordinate descent and still need to move between coordinate selection strategies

--- optimizers/coordinate.py
+++ optimizers/coordinate.py
@@ -1,28 +1,63 @@
 import numpy as np
-from loss import *
+from .loss import *
 
 class Coordinate_Based_Optimizers:
 
-	def __init__(batch_size,learning_rate,epsilon):
+	def __init__(self,learning_rate,epsilon,max_iters):
 
-		self.batch_size = batch_size
 		self.gamma = learning_rate
 		self.epsilon = epsilon
+		self.max_iters = max_iters
 
 	
 	def uniform_random_sampling(self,weight):
 
-		raise NotImplementedError
+		dimension = weight.shape[0] # column vector
+		return int(np.random.uniform(0,dimension))
 
-	def importance_sampling(self,weight):
+	def importance_sampling(self,weight,coordinate_smoothness):
 
-		raise NotImplementedError
+		dimension = weight.shape[0]
+		possible_coordinates = np.arange(0,dimension)
+		probabilities = coordinate_smoothness / np.sum(coordinate_smoothness)
+		return int(np.random.choice(possible_coordinates,p= probabilities)) 
 
-	def steepest_coordinate(self,weight):
+	def steepest_coordinate(self,gradient):
 
-		raise NotImplementedError
+		return np.argmax(gradient,axis = 1)
 	
+	
+	def select_coordinate(self,A,strategy,params):
 
-	def coordinate_descent(self,A,b):
+		if strategy == 'random' :
+			raise NotImplementedError
+		elif strategy == 'importance' :
+			raise NotImplementedError
+		elif strategy == 'steep':
+			raise NotImplementedError
+		else :
+			raise raise ValueError('Method not supported')
 
-		raise NotImplementedError
+	def coordinate_descent(self,A,b,strategy,params):
+
+		losses = []
+		iterator = 0
+		weight = np.zeros((A.shape[1],1))
+		while iterator < self.max_iters:
+
+			
+			index = self.uniform_random_sampling(weight)
+			basis_vector = np.ones_like(weight)
+			coordinate_weight = weight[index] * basis_vector
+			data_coordinate = (A[:,index]).reshape((-1,1))
+			gradient = first_order_least_squares(data_coordinate.T,coordinate_weight,b)
+			weight = weight - self.gamma * gradient
+			current_loss = least_squares_loss(A,weight,b)
+			losses.append(current_loss)
+			iterator += 1
+			
+
+		return weight,losses
+
+
+
